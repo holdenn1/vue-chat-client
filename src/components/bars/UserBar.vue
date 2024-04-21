728c5bdca39650dd16de49c09db04bf9bb8b6c68
Ihor Ivliev <caulfieldd17@gmail.com>
2024-04-21 21:21:33 +0300

    new commit t

--- src/components/bars/UserBar.vue
+++ src/components/bars/UserBar.vue
@@ -61,10 +61,15 @@ const updateNickname = async () => {
       toastify('warning', 'Nickname must be longer than 3 characters')
       return
     }
+
+
+
     const { data }: { data: User } = await updateUserRequest({ nickname: trimNickname })
     if (!data) {
       throw new Error()
     }
+
+    console.log(10000)
     userStore.setUser(data)
   } catch (e) {
     console.error(e)
